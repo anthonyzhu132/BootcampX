bd389ef976bd8ec0c1e9c5ca59f115b382c1d671
Anthony <anthonyzhu132@gmail.com>
2019-09-18 23:08:41 +0000

    added SQL injection prevention

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
-
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -13,10 +15,10 @@ FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 ORDER BY teacher
-LIMIT 10;
-`)
+LIMIT $2;
+`, values)
   .then((response) => {
     console.log(response.rows);
   });
